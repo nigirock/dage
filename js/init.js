2f7addcd285656b86080df2869762cf77dd6f800
nigirock <nigirock124@gmail.com>
2017-10-11 20:15:23 +0300

    обновление

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 (function($){
   $(function(){
-    $(".button-collapse").sideNav();
-    $('.carousel').carousel();
+
+    $('.button-collapse').sideNav();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
