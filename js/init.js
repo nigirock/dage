d10a112e8db79ea3d55d31fa6eb060c304c12f94
nigirock <nigirock124@gmail.com>
2017-10-17 14:05:29 +0300

    галерея

--- js/init.js
+++ js/init.js
@@ -1,7 +1,20 @@
 (function($){
   $(function(){
 
+
     $('.button-collapse').sideNav();
+    $('.slider').slider();
+
+    $("[data-fancybox]").fancybox({
+      // Options will go here
+    });
+    $('.bw').hover(function () {
+        $(this).addClass("animated pulse");
+    },function () {
+        $(this).removeClass("animated pulse");
+    })
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
